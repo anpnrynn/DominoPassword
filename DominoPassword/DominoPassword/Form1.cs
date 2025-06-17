e9f6e1be1944b54a89adcdacac08cc96eb0b3b51
Anoop Kumar Narayanan <anoop.kn@live.in>
2025-06-17 21:43:01 +0530

    Add files via upload

--- DominoPassword/DominoPassword/Form1.cs
+++ DominoPassword/DominoPassword/Form1.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Windows.Forms.AxHost;
 
 namespace DominoPassword
 {
@@ -10,7 +14,10 @@ namespace DominoPassword
 
         //Die Rahul Gandhi, you stealing french bastard!!!
 
-        private string initdata = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!$'*+,-./:;<=>?^_`~$'*+,-./:;<=>?^_`";
+        private string initdata     = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!$'*+,-./:;<=>?^_`~$'*+,-./:;<=>?^_`";
+        private string initdata_1_1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!$'*+,-./:;<=>?^_`~$'*+,-./:;<=>?^_`";
+        private string initdata_2_0 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()_+-={}|[]\\\\:\\\";'<>?,./^&*()_+-={}|[]\";";
+        private string initdata_2_1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()_+-={}|[]\\\\:\\\";'<>?,./^&*()_+-={}|[]\";"; // 108 characters
 
         private string shuffledData = "";
         private int[] toggleStatus = new int[128];
@@ -22,7 +29,62 @@ namespace DominoPassword
             return s2 + s1;
         }
 
-        private string swapchars(string s, int m, int n)
+
+        private void copyto(ref char[] array, char[] copyfrom, int offset, int n)
+        {
+            int i = 0;
+            while( i < copyfrom.Length)
+            {
+                array[offset+i] = copyfrom[i];
+                i++;
+            }
+        }
+        private char[] rotate_array(ref char[] srotate, int n)
+        {
+            char[] s1 = srotate.Take(n).ToArray();
+            char[] s2 = srotate.Skip(n).ToArray();
+            copyto(ref srotate, s2, 0, s2.Length);
+            copyto(ref srotate, s1, s2.Length, s1.Length );
+            return srotate;
+        }
+
+        private char[] trirotate_array(ref char[] srotate, int m, int n)
+        {
+            char[] s1 = srotate.Take(m).ToArray();
+            char[] s2 = srotate.Skip(m).ToArray().Take(n).ToArray();
+            char[] s3 = srotate.Skip(m + n).ToArray();
+
+            copyto(ref srotate, s2, 0, s2.Length);
+            copyto(ref srotate, s3, s2.Length, s3.Length);
+            copyto(ref srotate, s1, s2.Length+s3.Length, s1.Length);
+            return srotate;
+        }
+
+        string gen2_1_3wayrotate(string srotate, int count, int n)
+        {
+            string s1 = srotate.Substring(0, count);
+            string s2 = srotate.Substring(count, n);
+            string s3 = srotate.Substring(count + n, srotate.Length - count - n);
+            //string s2 = srotate.Substring(count, srotate.Length - count);
+            //memcpy(tmpdata, data, count);
+            //memcpy(data, &data[count], n - count);
+            //memcpy(&data[n - count], tmpdata, count);
+            return s2 + s3 + s1;
+        }
+
+        string gen2_1_rotate(string srotate, int count, int n)
+        {
+            string s1 = srotate.Substring(0, count);
+            string s2 = srotate.Substring(count, n-count);
+            //string s3 = srotate.Substring(count+n, srotate.Length-count-n);
+            //string s2 = srotate.Substring(count, srotate.Length - count);
+            //memcpy(tmpdata, data, count);
+            //memcpy(data, &data[count], n - count);
+            //memcpy(&data[n - count], tmpdata, count);
+            return s2 + s1;
+        }
+
+        private string swapchars(string s, int m, int n, int o = 0)
         {
             StringBuilder sb = new StringBuilder(s);
             char c = s[m];
@@ -31,7 +93,73 @@ namespace DominoPassword
             return sb.ToString();
         }
 
-        private string shuffle(string s, int pin)
+        private char[] swapchars_array(char[] s, int m, int n, int o = 0)
+        {
+            //StringBuilder sb = new StringBuilder(s);
+            char c = s[m];
+            s[m] = s[n];
+            s[n] = c;
+            return s;
+        }
+
+        private char[] swapchars_array_skip(char[] s, int skip, int m, int n, int o = 0)
+        {
+            //StringBuilder sb = new StringBuilder(s);
+            char c = s[skip+m];
+            s[skip + m] = s[skip + n];
+            s[skip + n] = c;
+            return s;
+        }
+
+        static int[] rotatevalues = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 0 };
+        //static int[] rotatevalues = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 0 };
+        private int gen1_1_rotatevalue(int pin, int[] shiftkeys)
+        {
+            int i = 0;
+            
+            while (shiftkeys[i] != 0)
+            {
+                if (pin < shiftkeys[i])
+                {
+                    return 0;
+                }
+                else
+                {
+
+                    if (pin % shiftkeys[i] == 0)
+                    {
+                        return rotatevalues[i];
+                    }
+                }
+                i++;
+            }
+            return 0;
+        }
+
+
+        int gen2_1_rotatevalue(int pin, int[] shiftkeys)
+        {
+            int i = 0;
+            
+            while (shiftkeys[i] != 0)
+            {
+                if (pin < shiftkeys[i])
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (pin % shiftkeys[i] == 0)
+                    {
+                        return rotatevalues[i];
+                    }
+                }
+                i++;
+            }
+            return 0;
+        }
+
+        private void shuffle(string s, int pin)
         {
             string stemp = s;
             int n = stemp.Length;
@@ -40,7 +168,6 @@ namespace DominoPassword
             {
                 string s1 = stemp.Substring(0, n / 2);
                 string s2 = stemp.Substring(n / 2, n / 2);
-
                 for (int i = 0; i < n / 4; i++)
                 {
                     if (k % 2 == 0)
@@ -53,41 +180,107 @@ namespace DominoPassword
                         s1 = swapchars(s1, i, n / 2 - i - 2);
                         s2 = swapchars(s2, i, n / 2 - i - 2);
                     }
-
                     k++;
                 }
                 s1 = rotate(s1, 13);
                 s2 = rotate(s2, 17);
                 stemp = s1 + s2;
-
-                if (s == stemp)
-                {
-                    //console.log("strings match: " + stemp);
-                }
                 stemp = rotate(stemp, 23);
             }
+            shuffledData = stemp;
+        }
+
+        private int[] shiftkeys = { 3500, 301390, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 
+        void gen1_1_shuffle( string s, int pin ) {
+            Debug.WriteLine("String length is : " + s.Length);
+            int n = 108;
+            int k = 1;
+            int j = 0;
+            char[] schar = s.ToCharArray();
+            int [] random = { 23, 13, 17, 7, 11 };
+            for ( ;  j<pin; j++ ) {
+                char[] s1 = schar.Take(n / 2).ToArray();
+                char[] s2 = schar.Skip(n / 2).ToArray();
 
-            if (stemp.Length != s.Length)
-            {
+                for (int i = 0; i<n/4; i++ ){
+                    if(k % 2  == 0 ){
+                        s1 = swapchars_array(s1, i, n/2-i-1, n/2 );
+                        s1 = swapchars_array_skip(s1, random[3], i, n/2-i-1-random[2], n/2 );
+                        s2 = swapchars_array(s2, i, n/2-i-1, n/2 );
+                        s1 = swapchars_array_skip(s1, random[4], i, n/2-i-1-random[4], n/2 );
+                    } else {
+                        s1 = swapchars_array(s1, i, n/2-i-2, n/2 );
+                        s1 = swapchars_array_skip(s1, random[3], i, n/2-i-2-random[3], n/2 );
+                        s2 = swapchars_array(s2, i, n/2-i-2, n/2 );
+                        s1 = swapchars_array_skip(s1, random[4], i, n/2-i-2-random[4], n/2 );
+                    }
 
-                Debug.WriteLine("INFO: lengths don't match ");
-                Debug.WriteLine(stemp);
-                Debug.WriteLine(s);
+                    s1 = swapchars_array(s1, i, random[1] + i, n / 2);
+                    s2 = swapchars_array(s2, i, random[2] + i, n / 2);
+                    k++;
+                }
+
+                rotate_array(ref s1, random[1]);
+                rotate_array(ref s2, random[2]);
+                copyto(ref schar, s1, 0, s1.Length);
+                copyto(ref schar, s2, s1.Length, s2.Length);
+                trirotate_array(ref schar, random[0], n - random[0]);
+                trirotate_array(ref schar, random[3], n - random[3]);
+                trirotate_array(ref schar, random[3], random[4]);
+
+                int nrotate = 0;
+                if ((nrotate = gen1_1_rotatevalue(pin, shiftkeys)) != 0)
+                {
+                    trirotate_array( ref schar, nrotate, n-nrotate);
+                }
             }
-            else
-            {
-                Debug.WriteLine("INFO: lengths match ");
-                Debug.WriteLine(stemp);
+            //shuffledData = ;
+            int p = 0;
+            Debug.WriteLine("Shuffled data: ");
+            while ( p < 108) { 
+                Debug.Write(schar[p]);
+                p++;
             }
+            Debug.WriteLine("");
+            shuffledData = new string(schar);
+            Debug.WriteLine("Exit string length is: " + shuffledData.Length);
+            return;
+        }
 
-            string extendstring = stemp;
-            //Debug.WriteLine("INFO: Origstring = "+ s);
-            //Debug.WriteLine("INFO: Cryptostring = "+ extendstring);
-            shuffledData = extendstring;
-            //setShuffledData(shuffledData);
-            return extendstring;
+
+        void gen2_shuffle( string s , int pin) {
+            int n = 108;
+            int k = 1;
+            string stemp = s;
+            int []random = { 23, 13, 17 };
+            for (int j = 0; j<pin; j++ ) {
+                string s1 = stemp.Substring(0, n / 2);
+                string s2 = stemp.Substring(n / 2, n / 2);
+                for (int i = 0; i<n/4; i++ ){
+                    if(k % 2  == 0 ){
+                        s1 = swapchars(s1, i, n/2-i-1, n/2 );
+                        s2 = swapchars(s2, i, n/2-i-1, n/2 );
+                    } else { 
+                        s1 = swapchars(s1, i, n/2-i-2, n/2 );
+                        s2 = swapchars(s2, i, n/2-i-2, n/2 );
+                    }
+                    k++;
+                }  
+                s1 = gen2_1_rotate(s1,   random[1], n / 2);
+                s2 = gen2_1_rotate(s2,   random[2], n / 2);
+                stemp = s1 + s2;
+                stemp = gen2_1_rotate(stemp, random[0], n);  
+            }
+            shuffledData = stemp;
+            return;
+        }
+
+
+        void gen2_1_shuffle( string s, int pin ) {
+            gen1_1_shuffle(s, pin); //init string is different, but the algorithm is the same
+            return;
         }
 
         public Form1()
@@ -138,8 +331,24 @@ namespace DominoPassword
             {
                 MessageBox.Show("The number is too large. Please enter a smaller number.");
                 return;
+            }
+            if( listBox1.SelectedIndex == 0 )
+            {
+                shuffle(initdata, int.Parse(textBox1.Text));
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                gen1_1_shuffle(initdata_1_1, int.Parse(textBox1.Text));
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                gen2_shuffle(initdata_2_0, int.Parse(textBox1.Text));
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                gen2_1_shuffle(initdata_2_1, int.Parse(textBox1.Text));
             }   
-            shuffledData = shuffle(initdata, int.Parse(textBox1.Text) );
+            
         }
 
 
